f9858e037b588824475da7fb83f69e0e77310ea9
RicardoOliveira-5 <a49436@alunos.isel.pt>
2026-03-25 15:21:53 +0000

    new updates

--- src/main/kotlin/pt/unl/fct/iadi/novaevents/repository/ClubRepository.kt
+++ src/main/kotlin/pt/unl/fct/iadi/novaevents/repository/ClubRepository.kt
@@ -6,6 +6,6 @@ import pt.unl.fct.iadi.novaevents.model.Club
 
 interface ClubRepository : JpaRepository<Club, Long> {
 
-    @Query("SELECT DISTINCT c FROM Club c LEFT JOIN FETCH c.events e LEFT JOIN FETCH e.type")
+    @Query("SELECT DISTINCT c FROM Club c  JOIN FETCH c.events e  JOIN FETCH e.type")
     fun findAllWithEvents(): List<Club>
 }
